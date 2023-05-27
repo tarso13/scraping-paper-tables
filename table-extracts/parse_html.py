75b40951a5860b326c1023191bc345be20da979a
Tarso Keletzi <89407536+tarso13@users.noreply.github.com>
2023-05-27 18:26:42 +0300

    Extract title and datasets for each table

--- table-extracts/parse_html.py
+++ table-extracts/parse_html.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 links = [
     "https://iopscience.iop.org/article/10.1086/313034/fulltext/35878.text.html",
-    "https://www.aanda.org/articles/aa/full_html/2018/08/aa32766-18/aa32766-18.html",
+    "https://www.aanda.org/articles/aa/full_html/2018/08/aa32766-18/aa32766-18.html"
 ]
 
 def fetch_data(url):
@@ -16,13 +16,23 @@ def fetch_data(url):
         print(e, "while fetching", url)
 
 
-def get_table_data():
-    html = None
-    for link in links:
-        html = fetch_data(link)
-        soup = BeautifulSoup(html, "html.parser")
-        tables = soup.findAll("table")
-        for table in tables:
-            print(table)
+def get_tables(link):
+    print("Results for " + link)
+    html = fetch_data(link)
+    soup = BeautifulSoup(html, "html.parser")
+    tables = soup.findAll("table")
+    return tables    
 
-get_table_data()
+
+def get_table_data(table):
+    headers = [th.get_text() for th in table.find("tr").find_all("td")]
+    print(headers)
+    datasets = []   
+    for row in table.find_all("tr")[1:]:
+        dataset = [td.get_text() for td in row.find_all("td")]
+        print(dataset)
+
+for link in links:
+    tables = get_tables(link)
+    for table in tables:
+        get_table_data(table)
