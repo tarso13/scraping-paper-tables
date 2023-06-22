dd78e477810a5da48b99fc529b3200f7b69825d0
Tarso Keletzi <89407536+tarso13@users.noreply.github.com>
2023-06-22 21:07:58 +0300

    Extract url for aanda article and its contents
    Only one example in nasa_ads_tutorial.py

--- nasa_ads_tutorial.py
+++ nasa_ads_tutorial.py
@@ -12,12 +12,28 @@ token=get_password('nasa_ads_token.txt')
 # and retrieve only the first 100 results
 encoded_query = urlencode({"q": "'aanda'",
                            "fq": "year:[2018 TO 2023]",
-                           "fl": "title",
-                           "rows": 100
+                           "fl": "bibcode",
+                           "rows": 1
                           })
 
 # execute the query
-results = requests.get("https://api.adsabs.harvard.edu/v1/search/query?{}".format(encoded_query), \
+bibecode_results = requests.get("https://api.adsabs.harvard.edu/v1/search/query?{}".format(encoded_query), \
                        headers={'Authorization': 'Bearer ' + token})
 
-print(results.json())
+# Extract bibcode from json results
+json_results = bibecode_results.json()
+json_response = json_results['response']
+json_docs = json_response['docs']
+bibcode_kv = str(json_docs[0])
+position = bibcode_kv.find(": '")
+final_bibcode = bibcode_kv[position + 3 : bibcode_kv.find("'}")]
+
+# download the arXiv html
+url_results = requests.get(f'https://ui.adsabs.harvard.edu/link_gateway/{final_bibcode}/PUB_HTML') 
+
+# write the output to file
+with open('html_content.html', 'wb') as f:
+    f.write(url_results.content)
+
+# show where the ADS URL redirected to
+print(f'Url found for html: {url_results.url}')
