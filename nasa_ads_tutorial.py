95e9e8511d31fd9f1e684c293328503f5b1534e5
Tarso Keletzi <89407536+tarso13@users.noreply.github.com>
2023-06-22 09:48:33 +0300

    Add comments for the nasa api tutorial

--- nasa_ads_tutorial.py
+++ nasa_ads_tutorial.py
@@ -2,14 +2,21 @@ from password import get_password
 import requests
 from urllib.parse import urlencode
 
+# get token for nasa api
 token=get_password('nasa_ads_token.txt')
 
+# Encode query 
+# In this case: search for aanda in publications,
+# filter the results for specific period of time,
+# title is the return value of the record,
+# and retrieve only the first 100 results
 encoded_query = urlencode({"q": "'aanda'",
                            "fq": "year:[2018 TO 2023]",
                            "fl": "title",
                            "rows": 100
                           })
 
+# execute the query
 results = requests.get("https://api.adsabs.harvard.edu/v1/search/query?{}".format(encoded_query), \
                        headers={'Authorization': 'Bearer ' + token})
 
