d685a8941161250205aba19060e107dc33d7ad53
Tarso Keletzi <tkeletzi@gmail.com>
2023-07-13 10:21:14 +0300

    Update list of urls

--- table-extracts/extract_tables.py
+++ table-extracts/extract_tables.py
@@ -2,13 +2,13 @@ from parse_html import *
 
 # Links to extract data from 
 urls = [
-    'https://iopscience.iop.org/article/10.1086/313034/fulltext/35878.text.html',
     'https://www.aanda.org/articles/aa/full_html/2018/08/aa32766-18/aa32766-18.html',
     'https://www.aanda.org/articles/aa/full_html/2023/06/aa44220-22/aa44220-22.html', 
     'https://www.aanda.org/articles/aa/full_html/2023/06/aa44161-22/aa44161-22.html',
     'https://www.aanda.org/articles/aa/full_html/2016/02/aa27620-15/aa27620-15.html',
-    'https://www.aanda.org/articles/aa/full_html/2016/01/aa26356-15/aa26356-15.html'
-    'https://iopscience.iop.org/article/10.3847/1538-4357/acd250'
+    'https://www.aanda.org/articles/aa/full_html/2016/01/aa26356-15/aa26356-15.html',
+    'https://iopscience.iop.org/article/10.3847/1538-4357/acd250',
+    'https://iopscience.iop.org/article/10.3847/1538-3881/acdd6f'
 ]
 
 # Main function to download initial urls and extract data
@@ -16,7 +16,6 @@ def main():
     print('Start downloading process...')
     download_all_html_files('publications', urls)
     print('Start extracting process...')
-    extract_tables('publications')
     extract_tables('publications_aanda')
     extract_tables('publications_aanda/publications_aanda_tables')
     extract_tables('publications_iopscience')
