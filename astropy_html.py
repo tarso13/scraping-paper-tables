3066877ca5ec49a0f22a1ce4cbb3c8e5f970a5f9
Tarso Keletzi <tkeletzi@gmail.com>
2023-09-12 14:58:36 +0300

    Update astropy example with all mrts
    Filenames referenced in mrt_titles are locally saved in the working directory.

--- astropy_html.py
+++ astropy_html.py
@@ -1,13 +1,18 @@
-from bs4 import BeautifulSoup
+import time
 import astropy.io.ascii as ascii
 
 def main():
-    file = open('Toy model for the acceleration of blazar jets _ Astronomy & Astrophysics (A&A).html', 'r')
-    content = file.read()
-    soup_content = BeautifulSoup(content, 'html.parser')
-    table = soup_content.find('table')
-    table_parent = table.parent.parent 
-    astropy_table = ascii.read(content, format='html') 
-    print(astropy_table)
+    mrt_titles = ['ajacdd6ft1_mrt.txt','ajacdd6ft5_mrt.txt','apjacd250t2_mrt.txt']
+    # html_titles = ['EXPRES_IV_Two_Additional_Planets_Orbiting_ρ_Coronae_Borealis_Reveal_Uncommon_System_Architecture_IOPscience.html','Testing_the_Linear_Relationship_between_Black_Hole_Mass_and_Variability_Timescale_in_Low-luminosity_AGNs_at_Submillimeter_Wavelengths_-_IOPscience.html']
+    for mrt_title in mrt_titles:
+        file = open(mrt_title)
+        content = file.read()
+        start = time.time()
+        astropy_table = ascii.read(content, format='cds') 
+        end = time.time()
+        print(f'Content of {mrt_title}')
+        print(astropy_table)
+        print(f'Time needed to read {mrt_title} is {str(end - start)} seconds.')
+       
     
 main() 
